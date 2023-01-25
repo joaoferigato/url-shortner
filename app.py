e31ee94eab0347a1dbd379063bdf528c98bca423
Joao Ferigato <joaoferigato@gmail.com>
2023-01-25 15:32:28 -0300

    Revert "Adding cookies and API"
    This reverts commit cf7344f36732d801c751d7f25a1a18d749fd4177.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -9,7 +9,7 @@ app.secret_key = 'aklsjdflkajsdfklajsçdfkaj'
 
 @app.route('/')
 def home():
-    return render_template('home.html', codes=session.keys())
+    return render_template('home.html')
 
 
 @app.route('/your-url', methods=['GET', 'POST'])
@@ -35,7 +35,6 @@ def your_url():
 
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
-            session[request.form['code']] = True
         return render_template('your_url.html', code=request.form['code'])
     else:
         return redirect(url_for('home'))
@@ -57,11 +56,6 @@ def redirect_to_url(code):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
-
-
-@app.route('/api')
-def session_api():
-    return jsonify(list(session.keys()))
 
 
 if __name__ == "__main__":
